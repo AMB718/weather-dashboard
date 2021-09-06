256e1e318bbd9bcddb1f4133bb631f31fcfa34b2
Annalisa <annalisa.bauer1@gmail.com>
2021-09-06 12:58:50 -0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const form = document.queryCommandValue('#search-weather');
+// const recentSearches = document.querySelector('#recent-searches')
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+})
